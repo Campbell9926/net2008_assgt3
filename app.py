e04eba42b6e2cc61f6e6c5a4cc94f2892677df72
Campbell_Austin <aust0061@algonquinlive.com>
2021-11-08 13:45:43 -0500

    Commit for iproute addition to app.py for br105

--- app.py
+++ app.py
@@ -25,5 +25,14 @@ def interfaces():
 
     return dd
 
+@app.route('/iproutes')
+def iproutes():
+    try:
+        file.open("iproutes.txt")
+        readlines = file.readlines()
+    except:
+        return("cannot open file")
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
